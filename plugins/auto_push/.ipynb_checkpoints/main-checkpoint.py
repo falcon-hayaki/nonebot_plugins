fb1a4905c8aa139f3fd213d69f04b563580150a9
falcon-hayaki <allrightprincekin@outlook.com>
2021-09-28 10:29:30 +0000

    auto save

--- plugins/auto_push/.ipynb_checkpoints/main-checkpoint.py
+++ plugins/auto_push/.ipynb_checkpoints/main-checkpoint.py
@@ -5,7 +5,7 @@ from pytz import timezone
 from nonebot import scheduler
 from nonebot.log import logger
 
-@scheduler.scheduled_job('cron', second='*/10')
+@scheduler.scheduled_job('cron', hour='*')
 async def auto_push():
     os.system('git add .')
     os.system('git commit -m "auto save"')
