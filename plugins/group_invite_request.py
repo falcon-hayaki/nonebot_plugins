adcc031d6760a6f5e613d6ad8da96dd957577430
falcon-hayaki <allrightprincekin@outlook.com>
2022-03-31 20:00:00 +0800

    auto save

--- plugins/group_invite_request.py
+++ plugins/group_invite_request.py
@@ -6,7 +6,7 @@ from utils.deco import only_these_sub_type
 @only_these_sub_type('invite')
 async def group_invite_request(session: RequestSession):
     ctx = session.ctx
-    if ctx.user_id == 1511603275:
+    if ctx.user_id in [1511603275, 765991108]:
         await session.approve()
     else:
         await session.reject('仅做测试用')
--- plugins/miao_lang/main.py
+++ plugins/miao_lang/main.py
@@ -4,9 +4,20 @@ resource_path = 'resources/reply'
 from utils import deco
 from .miao import Miao
 
+miao = Miao()
+
 @on_command('reply', aliases=['喵喵', '说人话'], only_to_me=False)
 @deco.only_these_msg('group')
-@deco.only_these_group([1014696092])
 async def reply(session: CommandSession):
     message = session.ctx.raw_message
-    print(message)
+    command, text = parse_message(message)
+    if command == '喵喵':
+        await session.send(miao.encode(text))
+    elif command == '说人话':
+        await session.send(miao.decode(text))
+    
+def parse_message(message):
+    message_split = message.split(' ')
+    if not message_split:
+        return '', ''
+    return message_split[0], ' '.join(message_split[1:])
