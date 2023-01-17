1a8116501d577d03e944f11367f470949b3f6027
falcon-hayaki <allrightprincekin@outlook.com>
2023-01-17 14:52:08 +0800

    debug

--- bgtasks/main.py
+++ bgtasks/main.py
@@ -6,11 +6,14 @@ from plugins.scheduler_msg_queue import msg_queue
 
 class BGTasks():
     def __init__(self) -> None:
-        self.bot = get_bot()
         self.loop = asyncio.get_event_loop()
         self.enabled_tasks = [
             'test'
         ]
+
+    @property
+    def bot(self):
+        return get_bot()
 
     def run(self):
         for task in self.enabled_tasks:
@@ -21,5 +24,6 @@ class BGTasks():
 
     async def test(self, interval=5):
         while True:
+            print('test')
             await self.bot.send_group_msg(group_id=1014696092, message='test')
             await asyncio.sleep(interval)
