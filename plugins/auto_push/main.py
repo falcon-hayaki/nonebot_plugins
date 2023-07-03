58291a25cf0578b79ca5a32d93835381c0c804ab
falcon-hayaki <allrightprincekin@outlook.com>
2023-07-03 11:34:20 +0800

    disable plugins: reply; auto push

--- plugins/auto_push/main.py
+++ plugins/auto_push/main.py
@@ -5,10 +5,10 @@ from pytz import timezone
 from nonebot import scheduler
 from nonebot.log import logger
 
-@scheduler.scheduled_job('cron', hour='*/2')
-async def auto_push():
-    os.system('git add .')
-    os.system('git commit -m "auto save"')
-    os.system('git push')
-    time = datetime.now(tz=timezone("Asia/Shanghai"))
-    logger.info('Git push at {}'.format(time))
+# @scheduler.scheduled_job('cron', hour='*/2')
+# async def auto_push():
+#     os.system('git add .')
+#     os.system('git commit -m "auto save"')
+#     os.system('git push')
+#     time = datetime.now(tz=timezone("Asia/Shanghai"))
+#     logger.info('Git push at {}'.format(time))
--- plugins/reply/main.py
+++ plugins/reply/main.py
@@ -4,13 +4,13 @@ resource_path = 'resources/reply'
 from utils import deco
 from .reply import Reply
 
-@on_command('reply', patterns='.*', only_to_me=False)
-@deco.only_these_group([1014696092])
-@deco.only_these_msg('group')
-async def reply(session: CommandSession):
-    message = session.ctx.message
-    r = Reply(session.ctx.group_id)
-    await r.init_data()
-    msg = await r.get_reply(message)
+# @on_command('reply', patterns='.*', only_to_me=False)
+# @deco.only_these_group([1014696092])
+# @deco.only_these_msg('group')
+# async def reply(session: CommandSession):
+#     message = session.ctx.message
+#     r = Reply(session.ctx.group_id)
+#     await r.init_data()
+#     msg = await r.get_reply(message)
 
-    await session.send(msg)
+#     await session.send(msg)
